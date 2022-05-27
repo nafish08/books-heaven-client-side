713ea9eb965f1772e8ebaaacc2299b596cd907ed
nafish08 <md.nafish.sadik@gmail.com>
2022-05-28 02:17:07 +0600

    Responsivess added to navigation bar

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -12,33 +12,36 @@ const Header = () => {
     }
     return (
         <div>
-            <Navbar bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
                     <Navbar.Brand as={Link} to='/home'>Books Heaven</Navbar.Brand>
-                    <Nav className="ms-auto">
-                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                        {
-                            user ? <Nav.Link as={Link} to='/manageItems'>Manage Items</Nav.Link>
-                                : ''
-                        }
-                        {
-                            user ? <Nav.Link as={Link} to='/myItems'>My Items</Nav.Link>
-                                : ''
-                        }
-                        {
-                            user ? <Nav.Link as={Link} to='/addItems'>Add Items</Nav.Link>
-                                : ''
-                        }
-                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                            {
+                                user ? <Nav.Link as={Link} to='/manageItems'>Manage Items</Nav.Link>
+                                    : ''
+                            }
+                            {
+                                user ? <Nav.Link as={Link} to='/myItems'>My Items</Nav.Link>
+                                    : ''
+                            }
+                            {
+                                user ? <Nav.Link as={Link} to='/addItems'>Add Items</Nav.Link>
+                                    : ''
+                            }
+                            <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                        </Nav>
 
-                    <Nav>
-                        {
-                            user ? <button onClick={handleSignOut} className='btn btn-link text-decoration-none'>Logout</button>
-                                :
-                                <Nav.Link as={Link} to='/login'>Login</Nav.Link>
-                        }
-                    </Nav>
+                        <Nav>
+                            {
+                                user ? <button onClick={handleSignOut} className='btn btn-link text-decoration-none'>Logout</button>
+                                    :
+                                    <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
