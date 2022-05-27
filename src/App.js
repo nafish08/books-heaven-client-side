10543abe79f387d41cc19f63dfa4381392d567d9
nafish08 <md.nafish.sadik@gmail.com>
2022-05-27 14:44:30 +0600

    Bootstrap installed and Header section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Pages/Home/Home';
+import Footer from './components/Shared/Footer/Footer';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello Viewers</h2>
+    <div className="">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
